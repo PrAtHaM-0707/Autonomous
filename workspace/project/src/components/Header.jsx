430df990633aae20406d32f28999a4e57888da4e
Pratham Mittal <prathamm4402@gmail.com>
2026-02-25 00:31:30 +0530

    Deleted Useless files

--- workspace/project/src/components/Header.jsx
+++
@@ -1,6 +0,0 @@
-// Cleaned up Header styles
-import React from "react";
-
-const Header = () => <header>Header</header>;
-
-export default Header;
--- workspace/project/src/components/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Components Index
- * 
- * ✅ ALLOWED FILE - Components Module
- * Export all UI components from a single entry point.
- */
-
-export { default as Button } from './Button.jsx';
-export { default as Card } from './Card.jsx';
